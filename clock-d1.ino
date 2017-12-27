aa0e72dc8e143c078e7cb357dca0164a8db35d91
ufocombat <ufocombat@gmail.com>
2017-12-27 17:56:45 +0300

    +

--- clock-d1.ino
+++ clock-d1.ino
@@ -4,35 +4,49 @@
 #include "RTClib.h"
 #include "AlarmLib.h"
 
+#define PIR_PIN D2  // Пир датчик
 
 RTC_DS1307 rtc;
 LiquidCrystal_I2C lcd(0x3F,16,2); // 1-Выяснить адрес дисплея
 
-#define PIR_PIN D2  // Пир датчик
-
 /* Плата D1
  * SCL - дисплея и часов
  * SDA - дисплея и часов - подкючаются параллельно
+ * Bluetooth 
+ * Tx->Rx D1
+ * Rx->Tx D0
 */
 
-volatile boolean disp_light = true;
+boolean disp_light = true;
+int lSec = 0;
+unsigned long time;
+boolean colon=true;
+
+int am[] = {    
+  11*60,        
+  13*60,    
+  18*60  
+};
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);//Off for D1
-
   pinMode(PIR_PIN, INPUT);
 
   Serial.begin(9600);
-//  Serial.println("Clock...");
-
-  lcd.init();                     
-  lcd.backlight();
 
 //  if (! rtc.begin()) {
 //    Serial.println("Couldn't find RTC");
 //    while (1);
 //  }
+  
+  Serial.print("Today: ");
+  Serial.println(Date2StrFull(rtc.now()));
+  Serial.println();
+
+  lcd.init();                     
+  lcd.backlight();
+
     
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //Установить часы как Дата Компиляции Скетча
   // January 21, 2014 at 3am you would call:
@@ -44,73 +58,51 @@ void CheckTheLigt(boolean f)
    if (f) lcd.backlight(); else lcd.noBacklight();
 }
 
-volatile int lSec = 0;
-unsigned long time;
-
-int val;
-
-int am[] = {    
-  11*60+15,        
-  13*60,    
-  18*60,
-  21*60
-};
- 
 void loop() {
  
  DateTime n = rtc.now();
  
   if (Serial.available())
   {
-    val = Serial.parseInt();      
-    Alarm a = Alarm(val);
+    Alarm alarm = Alarm(Serial.readString()); 
+    am[0] = alarm.minutes();
     
-    am[0] = val;
     Serial.print("Alarm on: ");
-    Serial.print(a.ToStr());        
+    Serial.println(alarm.ToStr());        
     
-    Serial.print(" (");        
-    Serial.print(val);
-    Serial.println(")");    
-    
-    Serial.println("Now: ");
-    Serial.println(Date2Str(n)+" "+DayOfWeek(n)+" "+Time2Str(n));
+    Serial.print("Now: ");
+    Serial.println(Date2StrFull(n));
     Serial.println();
   }
-
-  
-
  
  if (lSec!=n.second())
  {
   lcd.setCursor(0, 0);
-  lcd.print(Time2Str(n));
+  lcd.print(Time2StrDDMM(n,colon));
+  colon = !colon;
 
-//  lcd.setCursor(0, 1);
-//  lcd.print(Today(n)+" "+DayOfWeek(n));
-
-  
   int nnn = n.minute()+n.hour()*60;
   boolean a=false;
 
   int i;
   for (i=0; i < (sizeof(am)/sizeof(int)); i++){
-    if (am[i]>=nnn)
+    if (am[i]>nnn)
     {
       a=true;
       break;
     }
   }
  
-  String l= "     ";
+  String toTime= "     ";
 
-int r=0;
-int h=0;
+  int r=0;
+  int h=0;
 
   if (a)  
   {
     int m = am[i]%60;
     DateTime b (n.year(),n.month(),n.day(),(int)((am[i]-m)/60),m,0);
+    
     r = b.minute();
     h= b.hour();
     
@@ -118,27 +110,23 @@ int h=0;
     
     if ((t.hours()>=0)&&(t.minutes()>=0))
     {
-      l = (String)t.hours()+":"+Norm2Str(t.minutes());
-    }
-    
-    if (t.hours()>9) l=" "+l;
+      String w = " "+(String)t.hours()+":"+Norm2Str(t.minutes());
+      if (w.length()<=5) toTime=w;
+    }   
   }
   
-  lcd.setCursor(16-l.length(), 0); 
-  lcd.print(l);
-
+  lcd.print(" "+(String)h+":"+Norm2Str(r)+toTime);
+  
   lcd.setCursor(0, 1);
-  lcd.print((String)h+":"+Norm2Str(r));
-
+  lcd.print(Date2Str(n)+" "+DayOfWeek(n));
 
   lSec=n.second();
  }
-else
-{
-  delay(300);
-} 
+  else
+  {
+    delay(300);
+  } 
  
   if (digitalRead(PIR_PIN)==1) time = millis();  
- 
   CheckTheLigt(disp_light= millis()-time<20000);    
 }
